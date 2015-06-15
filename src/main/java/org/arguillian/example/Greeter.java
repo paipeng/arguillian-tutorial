11a797b66100f253bfca94fcd4285b4f448626d9
Pai Peng <pai.peng@apparentmedia.de>
2015-06-15 14:47:00 +0200

    init project

---
+++ src/main/java/org/arguillian/example/Greeter.java
@@ -0,0 +1,23 @@
+package org.arguillian.example;
+
+import java.io.PrintStream;
+
+import javax.inject.Inject;
+
+public class Greeter {
+	private PhraseBuilder phraseBuilder;
+	
+	@Inject
+    public Greeter(PhraseBuilder phraseBuilder) {
+        this.phraseBuilder = phraseBuilder;
+    }
+	
+	public void greet(PrintStream to, String name) {
+        to.println(createGreeting(name));
+    }
+
+    public String createGreeting(String name) {
+        //return "Hello, " + name + "!";
+    	return phraseBuilder.buildPhrase("hello", name);
+    }
+}
---
+++ src/main/java/org/arguillian/example/PhraseBuilder.java
@@ -0,0 +1,22 @@
+package org.arguillian.example;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+public class PhraseBuilder {
+	private Map<String, String> templates;
+
+    public String buildPhrase(String id, Object... args) {
+        return MessageFormat.format(templates.get(id), args);
+    }
+
+    @PostConstruct
+    public void initialize() {
+    	System.out.println("PhraseBuilder initialize");
+        templates = new HashMap<String, String>();
+        templates.put("hello", "Hello, {0}!");
+    }
+}
